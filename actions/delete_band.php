4fb01082ba22d14296a69cb2cc7f766a82a44d00
chrisramey <cjramey@gmail.com>
2013-01-29 13:15:57 -0600

    Initial commit

---
+++ actions/delete_band.php
@@ -0,0 +1,22 @@
+<?php 
+session_start();
+
+// Read file into array
+$lines = file('../data/bands.csv',FILE_IGNORE_NEW_LINES);
+
+// Replace line with new values
+unset($lines[$_GET['linenum']]);
+
+// Create the string to write to the file
+$data_string = implode("\n",$lines);
+
+// Write the string to the file, overwriting the current contents
+$f = fopen('../data/bands.csv','w');
+fwrite($f,$data_string);
+fclose($f);
+
+$_SESSION['message'] = 'Your band has been deleted.';
+
+header('Location:../?p=list_bands');
+
+?>
